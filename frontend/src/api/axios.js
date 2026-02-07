4dffa649eea3608075d7bc1e47b9aa2c9791343b
Rupam Kumar Sarangi <138404984+Rupam18@users.noreply.github.com>
2026-02-07 12:06:28 +0530

    frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,24 +2,24 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://saas-platform-6x5a.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL || "https://saas-platform-brre.onrender.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 // Attach JWT token automatically
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+    (config) => {
+        const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
 export default api;
